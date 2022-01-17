5e63b71e4f40e9960da7aa2f99b49d0c34fdd56e
DarwishQuiambao-GHub2 <djquiambao18@gmail.com>
2022-01-16 23:21:28 -0800

    mergeSort practice

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        AddBinary binaryAdd = new AddBinary();
-        String str = binaryAdd.binAdd("1010", "1011");
-        System.out.println(str);
+//        AddBinary binaryAdd = new AddBinary();
+//        String str = binaryAdd.binAdd("1010", "1011");
+//        System.out.println(str);
+
     }
 }
