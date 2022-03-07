12fb08568b17945a703e3b14e2a09e6af5c1361e
Darwish <djquiambao.18@gmail.com>
2022-03-06 23:48:38 -0800

    added Smallest Subarray Greater Sum

---
+++ src/SmallestSubGreaterSum.java
@@ -0,0 +1,13 @@
+//Given an array of positive numbers and a positive number ‘S,’
+// find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’.
+// Return 0 if no such subarray exists.
+
+public class SmallestSubGreaterSum {
+    public SmallestSubGreaterSum(){}
+    //arr = is array, s is the num need to find subarray sum greater than or equal to.
+    public int findSum(int [] arr, int s)
+    {
+
+    }
+
+}
