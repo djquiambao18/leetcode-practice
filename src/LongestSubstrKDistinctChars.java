a956f46cd2445e793ec508f9c6cff49d34dbfbd0
Darwish Quiambao <darwishjeric.quiambao@sjsu.edu>
2022-03-13 00:19:14 -0800

    filled in deets

--- src/LongestSubstrKDistinctChars.java
+++ src/LongestSubstrKDistinctChars.java
@@ -1,7 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
 
 //Longest Substring with K Distinct Characters (medium)
 public class LongestSubstrKDistinctChars {
-    int lengthSubstr(String str){
+    int lengthSubstr(String str, int K){
+        int length = Integer.MIN_VALUE;
+        if(str != null && str.length() > 0){
+            Set<Character> set = new HashSet<Character>();
+            //Sliding window:
+            int startWindex = 0; //Window Index == Windex
 
+            for(int i = 0; i < str.length(); i++){
+                set.add(str.charAt(i));
+                while(set.size() > K) {
+                    length = Math.max(length, i - startWindex);
+
+
+                }
+
+            }
+
+        }
+        return length;
     }
 }
