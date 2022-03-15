db8c5f678f732d918f28840a48188e46197f02fe
Darwish Quiambao <darwishjeric.quiambao@sjsu.edu>
2022-03-14 22:58:50 -0700

    need revision; incorrect solution

--- src/LongestSubstrKDistinctChars.java
+++ src/LongestSubstrKDistinctChars.java
@@ -5,8 +5,8 @@ import java.util.Set;
 public class LongestSubstrKDistinctChars {
     int lengthSubstr(String str, int K){
         int length = Integer.MIN_VALUE;
-        if(str != null && str.length() > 0){
-            Set<Character> set = new HashSet<Character>();
+        if(str != null || str.length() > 0){
+            Set<Character> set = new HashSet<>();
             //Sliding window:
             int startWindex = 0; //Window Index == Windex
             for(int i = 0; i < str.length(); i++){
